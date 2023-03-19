5907fc8c7900a99bd7e99d212cc4724faa26943b
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2023-03-19 17:29:38 +0300

    fix declaration type for svg

--- src/app/types/declaration.d.ts
+++ src/app/types/declaration.d.ts
@@ -6,8 +6,7 @@ declare module "*.scss" {
 declare module "*.jpg";
 declare module "*.png";
 declare module "*.svg" {
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-
-  export default src;
+  import React from "react";
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
 }
