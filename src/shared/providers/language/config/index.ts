756d083428abdc677a0a7547d2a391e92568b126
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2023-03-21 07:49:18 +0300

    add dev mode for n18

--- src/shared/providers/language/config/index.ts
+++ src/shared/providers/language/config/index.ts
@@ -4,9 +4,13 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  fallbackLng: "ru",
-  debug: true,
-});
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "ru",
+    debug: process.env.MODE === "development",
+  });
 
 export default i18n;
