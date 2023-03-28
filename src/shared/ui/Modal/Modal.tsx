02795e5e79ed86ca43ea25d2c184d34d75711f3c
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2023-03-28 08:54:54 +0300

    finish modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { type PropsWithChildren } from 'react'
+import React, { type PropsWithChildren } from 'react'
 import classNames from 'classnames'
 
 import { Icon } from '@mui/material'
@@ -11,18 +11,26 @@ import styles from './style.module.scss'
 type ModalProps = PropsWithChildren<{ isOpen?: boolean, onClose: () => void }>
 
 export const Modal = (props: ModalProps) => {
-  const { isOpen, children } = props
+  const { isOpen, onClose, children } = props
 
-  const classNameModal = classNames(styles.Modal__Overlay, {
+  const classNameModal = classNames(styles.Modal, {
     [styles.Modal_open]: isOpen
   })
 
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const element = e.target as Element
+    if (element?.className !== styles.Modal__Overlay) {
+      return
+    }
+    onClose()
+  }
+
   return <Portal>
     <div className={classNameModal}>
-      <div className={styles.Modal__Overlay}>
-        <div className={classNames(styles.Modal__Wrapper)}>
+      <div className={styles.Modal__Overlay} onClick={handleOverlayClick}>
+        <div className={classNames(styles.Modal__Container)}>
           <div className={styles.Modal__Header}>
-            <button className={styles.Modal__CloseButton}>
+            <button className={styles.Modal__CloseButton} onClick={onClose}>
               <Icon fontSize="small" component={CloseIcon} />
             </button>
           </div>
