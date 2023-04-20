f53fae4666a1eb829a390c4e0419ca7624721d88
Aleksandr Avdeev <alexandrawdxy@gmail.com>
2023-04-20 08:12:24 +0300

    add structure for ArticleDetailPage

---
+++ src/pages/ArticleDetailPage/ui/ArticleDetailPage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+
+export const ArticleDetailPage = () => {
+  return (
+    <div>
+      <Typography fontSize={24}>ARTICLE DETAIL PAGE</Typography>
+    </div>
+  )
+}
