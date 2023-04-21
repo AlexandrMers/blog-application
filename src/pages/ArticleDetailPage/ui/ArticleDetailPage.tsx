632268559dad262d75a63e6c22fa1989e07fe2cc
Aleksandr Avdeev <alexandrawdxy@gmail.com>
2023-04-21 09:42:47 +0300

    add primitives detail view for article

--- src/pages/ArticleDetailPage/ui/ArticleDetailPage.tsx
+++ src/pages/ArticleDetailPage/ui/ArticleDetailPage.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Skeleton, Typography } from '@mui/material'
+import { Box, Skeleton, Typography } from '@mui/material'
 
-import { articleModel } from 'entities/Article'
-
-// TODO - сделать адекватные реэкспорты ...
 import {
+  articleModel,
   type IBlockCode,
   type IBlockImage,
   type IBlockText,
   type IBlockType,
-} from 'entities/Article/model/types/articleType'
+} from 'entities/Article'
+
+import styles from './styles.module.scss'
 
 const isTextBlock = (block: IBlockType): block is IBlockText => {
   return block.type === 'text'
@@ -28,65 +28,78 @@ export const ArticleDetailPage = () => {
 
   if (isFetching) {
     return (
-      <div>
+      <div className={styles.ArticleDetailPage}>
         <Skeleton width={160} />
         <Skeleton width={180} />
         <Skeleton width={180} />
-        <Skeleton variant="rectangular" width={500} height={500} />
+        <Skeleton variant="rectangular" width={718} height={500} />
 
         {new Array(15).fill(null).map((_, index) => (
-          <Skeleton key={index} width={500} />
+          <Skeleton key={index} width={718} />
         ))}
       </div>
     )
   }
 
   return (
-    <div
-      style={{
-        width: '100%',
-        maxWidth: 500,
-      }}
-    >
-      <Typography fontSize={24}>{data?.title}</Typography>
-      <Typography fontSize={20}>{data?.subtitle}</Typography>
-      <span>{data?.createdAt}</span>
+    <div className={styles.ArticleDetailPage}>
+      <Box>
+        <Typography fontSize={24}>{data?.title}</Typography>
+      </Box>
+
+      <Box mt="5px">
+        <Typography fontSize={20}>{data?.subtitle}</Typography>
+      </Box>
+
+      <Box mt="5px">
+        <span className={styles.ArticleDetailPage__CreatedAt}>
+          {data?.createdAt}
+        </span>
+      </Box>
 
       {data?.img && (
-        <img
-          style={{
-            width: '100%',
-            maxWidth: '100%',
-            display: 'block',
-          }}
-          src={data?.img}
-          alt="main"
-        />
+        <Box mt="15px">
+          <img
+            className={styles.ArticleDetailPage__Image}
+            src={data?.img}
+            alt="main"
+          />
+        </Box>
       )}
 
       {data?.blocks.map((block, index) => {
         if (isTextBlock(block)) {
           return (
-            <React.Fragment key={index}>
-              <p>{block.title}</p>
+            <Box className={styles.ArticleDetailPage__TextBlock} key={index}>
+              <b>{block.title}</b>
               {block.paragraphs.map((paragraph, index) => (
                 <p key={index}>{paragraph}</p>
               ))}
-            </React.Fragment>
+            </Box>
           )
         }
 
         if (isImageBlock(block)) {
           return (
             <React.Fragment key={index}>
-              <p>{block.title}</p>
-              <img src={block.src} alt={block.src} />
+              <b>{block.title}</b>
+              <Box>
+                <img
+                  className={styles.ArticleDetailPage__Image}
+                  src={block.src}
+                  alt={block.src}
+                />
+              </Box>
             </React.Fragment>
           )
         }
 
         if (isCodeBlock(block)) {
-          return <code key={index}>{block.code}</code>
+          return (
+            <pre key={index} className={styles.ArticleDetailPage__CodeBlock}>
+              <code key={index}>{block.code}</code>
+            </pre>
+          )
         }
 
         return <></>
